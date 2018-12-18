b4580982012e1589e6256223d51bb4317a55de7e
RyzeZhao <643080112@qq.com>
2018-12-18 17:53:31 +0800

    模块1：下载URL的Picture

---
+++ crawler-test/src/main/java/com/zhaolearn/crawlertest/crawlerUrl/DownURLPicture.java
@@ -0,0 +1,36 @@
+package com.zhaolearn.crawlertest.crawlerUrl;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.io.File;
+import java.io.IOException;
+
+public class DownURLPicture {
+    public static void downloadPicture(String urlList, String path) {
+        URL url = null;
+        try {
+            url = new URL(urlList);
+            DataInputStream dataInputStream = new DataInputStream(url.openStream());
+
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(path));
+            ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = dataInputStream.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+            fileOutputStream.write(output.toByteArray());
+            dataInputStream.close();
+            fileOutputStream.close();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
