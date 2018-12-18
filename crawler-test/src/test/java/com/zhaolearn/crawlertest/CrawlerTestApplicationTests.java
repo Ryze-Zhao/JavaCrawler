8a9d84e47d6476cf61297be9ac78dc51036a9fd5
RyzeZhao <643080112@qq.com>
2018-12-18 17:53:42 +0800

    模块1：测试方法

---
+++ crawler-test/src/test/java/com/zhaolearn/crawlertest/CrawlerTestApplicationTests.java
@@ -0,0 +1,37 @@
+package com.zhaolearn.crawlertest;
+
+import com.zhaolearn.crawlertest.crawlerUrl.CrawlerTools;
+import com.zhaolearn.crawlertest.crawlerUrl.DownURLPicture;
+import com.zhaolearn.crawlertest.crawlerUrl.URLEntityRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrawlerTestApplicationTests {
+    @Autowired
+    private CrawlerTools crawlerTools;
+    @Test
+    public void crawlerAndDown() {
+        crawlerTools.crawlerAndDown("https://www.bilibili.com/","D:/Crawler");
+    }
+    @Test
+    public void spider() {
+        CrawlerTools.spider("https://www.bilibili.com/");
+    }
+    @Test
+    public void testDown() {
+        DownURLPicture.downloadPicture("https://i0.hdslb.com/bfs/sycp/creative_img/201901/93004fc1c8b8fedf261d38928604905e.jpg","d:/pic.jpg");
+    }
+    @Test
+    public void replace() {
+        DownURLPicture.downloadPicture("https://i0.hdslb.com/bfs/sycp/creative_img/201901/93004fc1c8b8fedf261d38928604905e.jpg","d:/pic.jpg");
+    }
+
+
+
+}
+
