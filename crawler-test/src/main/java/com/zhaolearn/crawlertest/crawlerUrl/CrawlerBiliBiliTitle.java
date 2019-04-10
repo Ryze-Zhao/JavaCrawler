f8f9cd556b1f6163f2af50ba4f34700794681ae5
RyzeZhao <643080112@qq.com>
2019-04-10 17:45:44 +0800

    注释

--- crawler-test/src/main/java/com/zhaolearn/crawlertest/crawlerUrl/CrawlerBiliBiliTitle.java
+++ crawler-test/src/main/java/com/zhaolearn/crawlertest/crawlerUrl/CrawlerBiliBiliTitle.java
@@ -23,18 +23,22 @@ public class CrawlerBiliBiliTitle {
         Document doc = null;
         try {
             logger.info("1111111111");
-            doc = Jsoup.connect(url).get();
-//            logger.error("----"+doc.outerHtml());
+            //因为bilibili的代码，并不是将视频列表的标题放在标签中，而是在头部有，所以创建好正则，一会从页面代码拿
             String regex = "\"part\":\"(.*?)\",\"duration\":";
             List<String> list = new ArrayList<String>();
             List<String> extvounoLists = new ArrayList<String>();
             Pattern pattern = Pattern.compile(regex);
+            // 先拿到整个页面
+            doc = Jsoup.connect(url).get();
+            //再拿到页面代码
             Matcher m = pattern.matcher(doc.outerHtml());
+            //根据页面代码使用正则分隔出来
             while (m.find()) {
                 int i = 1;
                 list.add(m.group(i));
                 i++;
             }
+            //只拿标题中的.然后只拿标题，如11.呵呵呵呵呵，这样就只拿到 呵呵呵呵呵
             for (String str : list) {
                 System.out.println("-----"+str);
                 String[] strs = str.split("\\.");
@@ -42,6 +46,7 @@ public class CrawlerBiliBiliTitle {
                 extvounoLists.add(strs[strs.length-1]);
             }
 int pN=0;
+            //输出
             for (String str : extvounoLists) {
                 pN++;
                 System.out.println(str+"（视频P"+pN+")");
